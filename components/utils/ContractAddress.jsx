8ac27acf31b2cd1192b24f9cd2ad26114956cb60
mickael misiti <mickael.misiti@acseo.fr>
2022-07-19 04:57:19 +0200

    Heroku

--- components/utils/ContractAddress.jsx
+++ components/utils/ContractAddress.jsx
@@ -1,1 +1,1 @@
-export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+export const contractAddress = '0xff4946b59be2d5875589ECd7d39975cE93f1E179';
--- components/utils/ContractOwner.jsx
+++ components/utils/ContractOwner.jsx
@@ -1,1 +1,1 @@
-export const owner = process.env.NEXT_PUBLIC_OWNER;
+export const owner = '0x3e7729aBcE92aA0d91C7aF994ebb2461d2ea2dF2';
